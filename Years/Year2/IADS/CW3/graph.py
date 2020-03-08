e2afd5b0fa99d0b67dc71ae130c39781e9d8e303
makspll <makspl17@gmail.com>
2020-03-08 20:20:18 +0000

    completed A IADS CW

--- Years/Year2/IADS/CW3/graph.py
+++ Years/Year2/IADS/CW3/graph.py
@@ -13,24 +13,99 @@ class Graph:
     # self.perm, self.dists, self.n are the key variables to be set up.
     def __init__(self,n,filename):
 
+        # open file reader
+        f = open(filename,"r")
 
+        # euclidian mode
+        if n == -1:
+            self.n = 0
+            nodes = []
+
+            for line in f:
+                lineStripped = line.strip()
+
+                if lineStripped == '':
+                    f.close()
+                    break
+                else:
+                    point = line.split()
+
+                    x = float(point[0])
+                    y = float(point[1])
+
+                    nodes.append((x,y))
+                    self.n += 1
+
+            self.dists = [[0]*self.n for x in range(self.n)]
+            # calculate distances between each node 
+
+            # use the fact dists will be a symmetric square matrix
+            # set the upper right triangle parallel to the lower left triangle
+            for i in range(self.n):
+                # diagonals will be untouched
+                for j in range(i):                    
+                    
+                    distItoJ = euclid(nodes[i],nodes[j])
+
+                    self.dists[i][j] = self.dists[j][i] = distItoJ
+        
+        # non-euclidian mode
+        else:
+            # assuming that all n(n+1)/2 edges are provided, and edges are symmetric
+            self.n = n
+            self.dists = [[0]*self.n for x in range(self.n)]
+
+
+            for line in f:
+                lineStripped = line.strip()
+
+                if lineStripped == '':
+                    f.close()
+                    break
+                else:
+                    values = line.split()
+
+                    i = int(values[0])
+                    j = int(values[1])
+                    dist = int(values[2])
+
+                    self.dists[i][j] = self.dists[j][i] = dist
+
+        # identity permutation perm[i] = i
+        self.perm = [x for x in range(self.n)]
+        print(self.perm)
     # Complete as described in the spec, to calculate the cost of the
     # current tour (as represented by self.perm).
     def tourValue(self):
+        return sum([ self.dists[self.perm[i]][self.perm[(i+1) % self.n]] for i in range(self.n)])
 
     # Attempt the swap of cities i and i+1 in self.perm and commit
     # commit to the swap if it improves the cost of the tour.
     # Return True/False depending on success.
     def trySwap(self,i):
+        # we only need to consider the impact on the "sub-tour" between i-1 and i+2
+        # effectively only the cost between i-1 and i, i and i+1, i+1 and i+2 changes
+        
+        prevN = self.perm[(i-1) % self.n]
+        currN = self.perm[i]
+        nextN = self.perm[(i + 1) % self.n]
+        sndNextN = self.perm[(i + 2) % self.n]
 
+        costInitial = self.dists[prevN][currN]  + self.dists[nextN][sndNextN]
+        costAfter = self.dists[prevN][nextN] + self.dists[currN][sndNextN]
+
+        if(costAfter < costInitial):
+            nxtIdx = (i+1) % self.n
+            self.perm[i],self.perm[nxtIdx] = self.perm[nxtIdx], self.perm[i]
+            return True
+        else:
+            return False
 
     # Consider the effect of reversiing the segment between
     # self.perm[i] and self.perm[j], and commit to the reversal
     # if it improves the tour value.
     # Return True/False depending on success.              
-    def tryReverse(self,i,j):
-
-
+    #def tryReverse(self,i,j):
     def swapHeuristic(self):
         better = True
         while better:
@@ -39,17 +114,18 @@ class Graph:
                 if self.trySwap(i):
                     better = True
 
-    def TwoOptHeuristic(self):
-        better = True
-        while better:
-            better = False
-            for j in range(self.n-1):
-                for i in range(j):
-                    if self.tryReverse(i,j):
-                        better = True
+    # def TwoOptHeuristic(self):
+    #     better = True
+    #     while better:
+    #         better = False
+    #         for j in range(self.n-1):
+    #             for i in range(j):
+    #                 if self.tryReverse(i,j):
+    #                     better = True
                 
 
     # Implement the Greedy heuristic which builds a tour starting
     # from node 0, taking the closest (unused) node as 'next'
     # each time.
-    def Greedy(self):
+    #def Greedy(self):
+
