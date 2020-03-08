e8736963102d8d9545a2d8e80f093c818b0f8153
makspll <makspl17@gmail.com>
2020-03-08 18:06:29 +0000

    started CW3

---
+++ Years/Year2/IADS/CW3/graph.py
@@ -0,0 +1,55 @@
+import math
+
+def euclid(p,q):
+    x = p[0]-q[0]
+    y = p[1]-q[1]
+    return math.sqrt(x*x+y*y)
+                
+class Graph:
+
+    # Complete as described in the specification, taking care of two cases:
+    # the -1 case, where we read points in the Euclidean plane, and
+    # the n>0 case, where we read a general graph in a different format.
+    # self.perm, self.dists, self.n are the key variables to be set up.
+    def __init__(self,n,filename):
+
+
+    # Complete as described in the spec, to calculate the cost of the
+    # current tour (as represented by self.perm).
+    def tourValue(self):
+
+    # Attempt the swap of cities i and i+1 in self.perm and commit
+    # commit to the swap if it improves the cost of the tour.
+    # Return True/False depending on success.
+    def trySwap(self,i):
+
+
+    # Consider the effect of reversiing the segment between
+    # self.perm[i] and self.perm[j], and commit to the reversal
+    # if it improves the tour value.
+    # Return True/False depending on success.              
+    def tryReverse(self,i,j):
+
+
+    def swapHeuristic(self):
+        better = True
+        while better:
+            better = False
+            for i in range(self.n):
+                if self.trySwap(i):
+                    better = True
+
+    def TwoOptHeuristic(self):
+        better = True
+        while better:
+            better = False
+            for j in range(self.n-1):
+                for i in range(j):
+                    if self.tryReverse(i,j):
+                        better = True
+                
+
+    # Implement the Greedy heuristic which builds a tour starting
+    # from node 0, taking the closest (unused) node as 'next'
+    # each time.
+    def Greedy(self):
