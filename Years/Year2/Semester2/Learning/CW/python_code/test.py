ef846961d3a6b1f16f6a1a6cde0c92c1233c7b58
makspll <makspl17@gmail.com>
2020-04-01 23:13:24 +0100

    updated graph

--- Years/Year2/Semester2/Learning/CW/python_code/test.py
+++ Years/Year2/Semester2/Learning/CW/python_code/test.py
@@ -12,8 +12,20 @@ matplotlib.rcParams.update({
 
 import task1_1
 
-# for determining the size of figures to port to latex
+# for easier colorbar layout
+def colorbar(mappable):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    import matplotlib.pyplot as plt
+    last_axes = plt.gca()
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = fig.colorbar(mappable, cax=cax)
+    plt.sca(last_axes)
+    return cbar
 
+# for determining the size of figures to port to latex
 def set_size(width, fraction=1, subplots=(1, 1)):
     """ Set figure dimensions to avoid scaling in LaTeX.
 
@@ -55,7 +67,7 @@ def set_size(width, fraction=1, subplots=(1, 1)):
     return (fig_width_in, fig_height_in)
 
 if __name__ == "__main__":
-    width = 345
+    width = 505
     
     print("task1_1,b)")
     data = scipy.io.loadmat("../data/dset.mat")
@@ -64,26 +76,27 @@ if __name__ == "__main__":
     outR = scipy.io.loadmat("t1_R.mat")["R"]
     outS = scipy.io.loadmat("t1_S.mat")["S"]
 
-    fig,axes = plt.subplots(2,1,figsize=set_size(width,subplots=(2,1)))
+    fig,axes = plt.subplots(nrows=2,ncols=1,figsize=set_size(width,subplots=(2,1)))
 
     im = axes[0].imshow(outS, interpolation="none", cmap='RdBu_r',origin='lower',vmin=-0.04,vmax=0.04)
-    axes[0].set_title("Covariance matrix of X's")
+    axes[0].set_title("Covariance Matrix")
     axes[0].set_xticks(np.arange(outS.shape[0]))
     axes[0].set_yticks(np.arange(outS.shape[0]))
     axes[0].tick_params(axis='both', which='major', labelsize=5)
     axes[0].set_xlabel("variable")
     axes[0].set_ylabel("variable")
-    plt.colorbar(im,ax=axes[0])
+    colorbar(im)
 
     im = axes[1].imshow(outR, interpolation="none", cmap='RdBu_r',origin='lower',vmin=-1,vmax=1)
-    axes[1].set_title("Correlation matrix of X's")
+    axes[1].set_title("Correlation Matrix")
     axes[1].set_xticks(np.arange(outR.shape[0]))
     axes[1].set_yticks(np.arange(outR.shape[0]))
     axes[1].tick_params(axis='both', which='major', labelsize=5)
     axes[1].set_xlabel("variable")
     axes[1].set_ylabel("variable")
-    plt.colorbar(im,ax=axes[1])
-    
+    colorbar(im)
+
     fig.tight_layout()
+
     plt.savefig('correlation.png')
     plt.show()
